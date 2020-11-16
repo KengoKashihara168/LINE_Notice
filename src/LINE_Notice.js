5e217221c89faafaf588b72710fb7cad11978a05
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-11-16 21:47:37 +0900

    LINEへテキストを送信できるように実装

--- src/LINE_Notice.js
+++ src/LINE_Notice.js
@@ -1,3 +1,17 @@
 function main(){
     console.log("Hello World!");
+    var text = "Test";
+    var token = "7vmMoX6tnTEoDxExqS4wYAInveeHRYp1Hia4WPoSrfZ";
+    var options = {
+        "method" : "post",
+        "headers" : {
+            "Authorization" : "Bearer "+ token
+        },
+        "payload" : {
+            "message" : text
+          }
+    };
+
+    var url = "https://notify-api.line.me/api/notify";
+    UrlFetchApp.fetch(url, options);
 }
